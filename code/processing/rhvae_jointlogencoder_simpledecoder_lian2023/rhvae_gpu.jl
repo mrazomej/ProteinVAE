8b52ab56475601c76ccb6badc248bb09577b9deb
mrazomej <manuel.razo.m@gmail.com>
2024-02-07 10:41:28 -0800

    first attempt to train rhvae on protein data

---
+++ code/processing/rhvae_jointlogencoder_simpledecoder_lian2023/rhvae_gpu.jl
@@ -0,0 +1,247 @@
+## =============================================================================
+println("Import packages")
+
+# Import CUDA
+using CUDA
+
+# Import project package
+import ProteinVAE as PV
+
+# Import library to list files
+import Glob
+
+# Import Dataframe-related libraries
+import XLSX
+import DataFrames as DF
+
+# Import ML Libraries
+import Flux
+import AutoEncode
+
+## =============================================================================
+
+# Define model hyperparameters
+
+# Define latent dimension
+latent_dim = 2
+# Define number of epochs
+n_epoch = 100
+# Define number of samples in batch
+n_batch = 32
+# Define learning rate
+η = 1.0f-3
+
+# Define RHVAE hyper-parameters
+T = 0.8f0 # Temperature
+λ = 1.0f-2 # Regularization parameter
+n_centroids = 25 # Number of centroids
+
+# Define loss function hyper-parameters
+K = 5 # Number of leapfrog steps
+ϵ = 1.0f-2 # Leapfrog step size
+βₒ = 0.3f0^2 # Initial temperature for tempering
+
+# Defie loss function keyword arguments
+loss_kwargs = Dict(
+    :K => K,
+    :ϵ => ϵ,
+    :βₒ => βₒ,
+)
+
+## =============================================================================
+
+println("Load data...\n")
+
+# List data directory
+data_dir = "$(git_root())/data/lian_2023"
+
+# List data files
+files = sort(Glob.glob("$(data_dir)/Library*.xlsx"[2:end], "/"))
+
+# Read excel file into DataFrame
+df = XLSX.readtable(last(files), "Sheet1") |> DF.DataFrame
+
+# Rename all columns to lowercase
+DF.rename!(df, Dict(zip(DF.names(df), lowercase.(DF.names(df)))))
+
+## =============================================================================
+
+println("One-hot encode sequences...\n")
+
+# One-hot encode sequences
+seq_onehot = PV.seq.seq_onehotbatch(String.(df.sequence_aligned))
+
+# Convert to Float32
+seq_onehot = Float32.(Array(seq_onehot))
+
+# Extract sequence length, number of unique characters, and number of sequences
+n_char, seq_len, n_seq = size(seq_onehot)
+
+# Select centroids via k-means
+centroids_data = AutoEncode.utils.centroids_kmeans(seq_onehot, n_centroids)
+
+## =============================================================================
+
+println("Define model...\n")
+
+# Define number input features
+n_input = n_char * seq_len
+# Define expansion size
+n_neuron = floor(Int64, n_input * 1.5)
+
+println("Define encoder...\n")
+
+# Define encoder mlp
+
+encoder_mlp = Flux.Chain(
+    # Flatten input
+    x -> Flux.flatten(x),
+    # Encoder first layer
+    Flux.Dense(n_input => n_neuron, Flux.leakyrelu),
+    # Add dropout layer
+    Flux.Dropout(0.3),
+
+    # Enconder second layer
+    Flux.Dense(n_neuron => n_neuron, Flux.leakyrelu),
+    # Add normalization layer
+    Flux.LayerNorm(n_neuron),
+
+    # Encoder third layer
+    Flux.Dense(n_neuron => n_neuron, Flux.leakyrelu),
+)
+
+# Define encoder µ and log(σ) layers
+encoder_µ = Flux.Dense(n_neuron => latent_dim, Flux.identity)
+encoder_logσ = Flux.Dense(n_neuron => latent_dim, Flux.identity)
+
+# Build encoder
+encoder = AutoEncode.JointLogEncoder(encoder_mlp, encoder_µ, encoder_logσ)
+
+## =============================================================================
+
+println("Define decoder...\n")
+
+decoder = AutoEncode.SimpleDecoder(
+    # Define decoder mlp
+    Flux.Chain(
+        # Decoder first layer
+        Flux.Dense(latent_dim => n_neuron, Flux.leakyrelu),
+        # Add normalization layer
+        Flux.LayerNorm(n_neuron),
+
+        # Decoder second layer
+        Flux.Dense(n_neuron => n_neuron, Flux.leakyrelu),
+        # Add Dropout layer
+        Flux.Dropout(0.3),
+
+        # Decoder third layer
+        Flux.Dense(n_neuron => n_neuron, Flux.leakyrelu),
+        # Add normalization layer
+        Flux.LayerNorm(n_neuron),
+
+        # Define output layer
+        Flux.Dense(n_neuron => n_input, Flux.identity),
+        # Reshape output
+        x -> reshape(x, (n_char, seq_len, :)),
+
+        # Apply softmax to output
+        x -> Flux.softmax(x, dims=1),
+    )
+)
+
+## =============================================================================
+
+println("Defining Metric MLP...\n")
+
+# Define metric mlp
+metric_mlp = Flux.Chain(
+    # Reshape input
+    x -> Flux.flatten(x),
+    # Metric first layer
+    Flux.Dense(n_input => n_neuron, Flux.leakyrelu),
+    # Add normalization layer
+    Flux.LayerNorm(n_neuron),
+
+    # Metric second layer
+    Flux.Dense(n_neuron => n_neuron, Flux.leakyrelu),
+    # Add normalization layer
+    Flux.LayerNorm(n_neuron),
+
+    # Metric third layer
+    Flux.Dense(n_neuron => n_neuron, Flux.leakyrelu),
+)
+
+# Define diag and lower layers
+diag = Flux.Dense(n_neuron => latent_dim, Flux.identity)
+lower = Flux.Dense(n_neuron => latent_dim * (latent_dim - 1) ÷ 2, Flux.identity)
+
+metric_chain = AutoEncode.RHVAEs.MetricChain(metric_mlp, diag, lower)
+
+## =============================================================================
+
+println("Define RHVAE...\n")
+
+rhvae = AutoEncode.RHVAEs.RHVAE(
+    encoder * decoder, metric_chain, centroids_data, T, λ
+) |> Flux.gpu
+
+## =============================================================================
+
+# Explicit setup of optimizer
+opt_rhvae = Flux.Train.setup(
+    Flux.Optimisers.Adam(η),
+    rhvae
+)
+
+## =============================================================================
+
+println("Setting output directories...")
+
+# Define output directory
+out_dir = "./output/$(n_epoch)_epoch"
+
+# Check if output directory exists
+if !isdir("./output/")
+    mkdir("./output/")
+end # if
+
+# Check if output directory exists
+if !isdir(out_dir)
+    mkdir(out_dir)
+end # if
+
+
+## =============================================================================
+
+println("Preparing DataLoader...")
+
+# Prepare DataLoader
+train_loader = Flux.DataLoader(
+    seq_onehot |> Flux.gpu, batchsize=n_batch, shuffle=true
+)
+
+## =============================================================================
+
+
+println("Training RHVAE...\n")
+# Loop through number of epochs
+for epoch in 1:n_epoch
+    println("\nEpoch: $(epoch)\n")
+    # Loop through batches
+    for (i, x) in enumerate(train_loader)
+        println("Batch: $(i) / $(length(train_loader))")
+        # Train RHVAE
+        AutoEncode.RHVAEs.train!(
+            rhvae, x, opt_rhvae; loss_kwargs=loss_kwargs, verbose=true
+        )
+    end # for train_loader
+    # Save checkpoint
+    JLD2.jldsave(
+        "$(out_dir)/rhvae_epoch$(epoch).jld2", model_state=Flux.state(rhvae)
+    )
+end # for n_epoch
+
+# Save final model
+JLD2.jldsave(
+    "$(out_dir)/rhvae_epoch$(epoch)_final.jld2", model_state=Flux.state(rhvae)
+)
